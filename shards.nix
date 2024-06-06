7bf83fff0fc3cd852104b97174d9f1c1896c4aa1
Peter Hoeg <peter@hoeg.com>
2024-06-06 11:04:24 +0200

    shards.nix

---
+++ shards.nix
@@ -0,0 +1,42 @@
+{
+  ameba = {
+    url = "https://github.com/crystal-ameba/ameba.git";
+    rev = "6d03cef6dfd0f52faf19bbcd47d14831db4d7834";
+    sha256 = "1gbpavswb03352y1vqh3rv9bxk4jy79yjlwv3vagvcd08380sjjx";
+  };
+  db = {
+    url = "https://github.com/crystal-lang/crystal-db.git";
+    rev = "v0.13.1";
+    sha256 = "02b79bdv5h460ay0vkpwi5q69b1qrm9z29z02p09xb03hijxskpd";
+  };
+  i18n = {
+    url = "https://github.com/crystal-i18n/i18n.git";
+    rev = "v0.2.1";
+    sha256 = "16valnsr3qyxa3cc0pzrix3pficgicji9s6hwnmy9qhnkmyq6ilj";
+  };
+  msgpack = {
+    url = "https://github.com/crystal-community/msgpack-crystal.git";
+    rev = "v1.3.4";
+    sha256 = "19l8lh9z0cx40hwx2855xnaka2makkdri8y3d2v1rf2mnzgh09p6";
+  };
+  mysql = {
+    url = "https://github.com/crystal-lang/crystal-mysql.git";
+    rev = "v0.16.0";
+    sha256 = "1g8cv8pacymxqbds3jpg6iqrfbfzij18r99s3fpjjx5xc3dnml0x";
+  };
+  pg = {
+    url = "https://github.com/will/crystal-pg.git";
+    rev = "v0.28.0";
+    sha256 = "0g1kdpnz7sr7hpama0y3s70v5xjhi7jm2ypb1w503cfvr30qffpm";
+  };
+  sqlite3 = {
+    url = "https://github.com/crystal-lang/crystal-sqlite3.git";
+    rev = "v0.21.0";
+    sha256 = "1mhg59l6qgmf14zjrais0jfn1h5jj9j32fy7fb0gkp9zhpa7x6vs";
+  };
+  timecop = {
+    url = "https://github.com/crystal-community/timecop.cr.git";
+    rev = "v0.5.0";
+    sha256 = "1qdza9jcihpwhgas967b1gwa1sklxhvijx8sy475qxr79praq5k1";
+  };
+}
